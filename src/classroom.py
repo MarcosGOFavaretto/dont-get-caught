cc5819513f40257105d16423cdf0e14265c89d37
pultzlucas <lucas.pultz@nappsolutions.com>
2025-03-13 13:27:29 -0300

    feat: adiciona a classe de mapa de sala e a classe professor

--- src/classroom.py
+++ src/classroom.py
@@ -1,7 +1,51 @@
-class Classroom:
-    def __init__(self, dimension: tuple[int, int], x: int, y: int, rows: int, columns: int):
-        self.dimension = dimension
-        self.rows = rows
-        self.columns = columns
+from .models import Coordinate
+
+class ClassroomMapPoint:
+    def __init__(self, row: int, column: int, x: int, y: int):
+        self.column = column
+        self.row = row
+        self.is_student_desk = self.is_student_desk_condition()
         self.x = x
         self.y = y
+
+    def is_student_desk_condition(self) -> bool:
+        return self.column % 2 == 0 and self.row % 2 != 0
+    
+    def __str__(self):
+        return f"ClassroomMapPoint({self.row}, {self.column}, {self.is_student_desk}, {self.x}, {self.y})"
+
+class Classroom:
+    def __init__(self, dimension: tuple[int, int], x: int, y: int, desk_rows: int, desk_columns: int):
+        self.width, self.height = dimension
+        self.desk_rows = desk_rows
+        self.desk_columns = desk_columns
+        self.map_rows = self.desk_rows * 2
+        self.map_columns = (self.desk_columns * 2) - 1
+        self.column_width = self.width / self.map_columns
+        self.row_width = self.height / self.map_rows
+        self.x = x
+        self.y = y
+        self.map_points = self.get_map_points()
+
+    # Função para criar um map point a partir de uma posição do mapa de sala
+    #
+    def map_point(self, row: int, column: int):
+        x = self.column_width * column + self.x
+        y = self.row_width * row + self.y
+        return ClassroomMapPoint(row, column, x, y)
+    
+    # Função para gerar os pontos de posição da sala de aula
+    #   Classroom 3 x 4 / Mapa de pontos 6 x 7
+    #   . . . . . . .       
+    #   @ . @ . @ . @       
+    #   . . . . . . .       @ - Carteira de aluno 
+    #   @ . @ . @ . @       . - Ponto de movimento
+    #   . . . . . . . 
+    #   @ . @ . @ . @ 
+    #
+    def get_map_points(self):
+        movement_points = list[ClassroomMapPoint]()
+        for r in range(self.map_rows):
+            for c in range(self.map_columns):
+                movement_points.append(self.map_point(r, c))
+        return movement_points
--- src/teacher.py
+++ src/teacher.py
@@ -1,13 +1,7 @@
-from models import Coordinate
-from classroom import Classroom
+from .models import Coordinate
+from .classroom import Classroom, ClassroomMapPoint
 import random
 from enum import Enum
-
-class TeacherMovementPoint(Coordinate):
-    def __init__(self, coordinate: Coordinate, row: int, column: int):
-        super().__init__(x=coordinate.x, y=coordinate.y)
-        self.column = column
-        self.row = row
 
 class MovementDirection(Enum):
     LEFT = 'LEFT'
@@ -16,31 +10,35 @@ class MovementDirection(Enum):
     DOWN = 'DOWN'
 
 class TeacherMovement:
-    def __init__(self, point: TeacherMovementPoint, direction: MovementDirection):
+    def __init__(self, point: ClassroomMapPoint, direction: MovementDirection):
         self.point = point
         self.direction = direction
 
+    def __str__(self):
+        return f"TeacherMovement({self.point}, {self.direction})"
+
 class Teacher:
-    def __init__(self, name: str, classroom: Classroom):
+    def __init__(self, name: str, classroom: Classroom, initial_position: ClassroomMapPoint):
         self.name = name
         self.classroom = classroom
-        self.classroom_movement_points = self.get_movement_points()
-        self.current_position: TeacherMovementPoint = TeacherMovementPoint(Coordinate(0, 0), 2, 1)
+        self.current_map_point_position: ClassroomMapPoint = initial_position if initial_position is not None \
+            else classroom.map_point(0, 0)
 
     # Dentre as possibilidades de movimento possíveis, é escolhida uma aleatoriamente.
     #     
     def next_movement(self) -> TeacherMovement:
         movement_possibilities = self.get_movement_possibilities()
         next_movement_point = random.choice(movement_possibilities)
-        movement_direction = self.get_movement_direction(self.current_position, next_movement_point)
+        movement_direction = self.get_movement_direction(self.current_map_point_position, next_movement_point)
+        self.current_map_point_position = next_movement_point
         return TeacherMovement(
-            point=next_movement_point,
-            direction=movement_direction
+            point=self.current_map_point_position,
+            direction=movement_direction,
         )
     
     # Retorna a direção do movimento a partir do ponto inicial e ponto final
     #
-    def get_movement_direction(self, start_point: TeacherMovementPoint, finish_point: TeacherMovementPoint) -> MovementDirection:
+    def get_movement_direction(self, start_point: ClassroomMapPoint, finish_point: ClassroomMapPoint) -> MovementDirection:
         if start_point.row < finish_point.row:
             return MovementDirection.DOWN
         elif start_point.row > finish_point.row:
@@ -50,53 +48,43 @@ class Teacher:
         elif start_point.column > finish_point.column:
             return MovementDirection.LEFT
 
-    # Regras para movimentação:
-    #   - O professor pode apenas se mover por todo o corredor da sala (mesmo movimento da torre no xadrez);
+    # Função para calcular o próximo movimento possível do professor:
+    #   Regras de movimento:
+    #   - O professor pode se mover por todo o corredor da sala (mesmo movimento da torre no xadrez);
+    #   - O professor não pode parar em cima de uma cateira;
     #   - O professor não pode pular por cima de uma carteira;
     #
-    def get_movement_possibilities(self) -> list[TeacherMovementPoint]:
-        possible_movements = list[TeacherMovementPoint]()
-        for point in self.classroom_movement_points:
-            is_same_row_or_column = point.row == self.current_position.row or point.column == self.current_position.column
-            is_not_jumping_desk = point.row >= self.current_position.row + 1 \
-                or point.column >= self.current_position.column + 1 \
-                or point.row <= self.current_position.row - 1 \
-                or point.column <= self.current_position.column - 1
-            is_valid = is_same_row_or_column and is_not_jumping_desk
-            if is_valid:
-                possible_movements.append(point)
+    def get_movement_possibilities(self) -> list[ClassroomMapPoint]:
+        column_map_points = [mp for mp in self.classroom.map_points if mp.column == self.current_map_point_position.column]
+        row_map_points = [mp for mp in self.classroom.map_points if mp.row == self.current_map_point_position.row]
+        possible_movements = list[ClassroomMapPoint]()
+
+        # up
+        for row_index in range(self.current_map_point_position.row, 0, -1):
+            point = column_map_points[row_index - 1]
+            if point.is_student_desk:
+                break
+            possible_movements.append(point)
+
+        # down
+        for row_index in range(self.current_map_point_position.row + 1, self.classroom.map_rows, 1):
+            point = column_map_points[row_index]
+            if point.is_student_desk:
+                break
+            possible_movements.append(point)
+
+        # left
+        for column_index in range(self.current_map_point_position.column, 0, -1):
+            point = row_map_points[column_index - 1]
+            if point.is_student_desk:
+                break
+            possible_movements.append(point)
+
+        # right
+        for column_index in range(self.current_map_point_position.column + 1, self.classroom.map_columns, 1):
+            point = row_map_points[column_index]
+            if point.is_student_desk:
+                break
+            possible_movements.append(point)
+
         return possible_movements
-
-
-    # Função para gerar os pontos de movimento do professor na sala
-    #   Classroom 4 x 3 / Mapa de pontos 7 x 6
-    #   . . . . . . .       
-    #   @ . @ . @ . @       
-    #   . . . . . . .       @ - Carteira de aluno 
-    #   @ . @ . @ . @       . - Ponto de movimento
-    #   . . . . . . . 
-    #   @ . @ . @ . @ 
-    #
-    def get_movement_points(self) -> list[TeacherMovementPoint]:
-        mv_points_columns = (self.classroom.columns * 2) - 1
-        mv_points_rows = self.classroom.rows * 2
-        x, y = self.classroom.dimension
-        column_size = x / mv_points_columns
-        row_size = y / mv_points_rows
-        movement_points = list[TeacherMovementPoint]()
-        for r in range(mv_points_rows):
-            for c in range(mv_points_columns):
-                point_is_desk = c % 2 == 0 and r % 2 != 0
-                if point_is_desk:
-                    continue
-                movement_points.append(TeacherMovementPoint(
-                    row=r,
-                    column=c,
-                    coordinate=Coordinate(column_size * c, row_size * r),
-                ))
-        return movement_points
-t = Teacher(name='Oliver Taz', classroom=Classroom(dimension=(1000, 1000), x=0, y=0, rows=3, columns=4))
-
-nx = t.next_movement()
-
-print('( row:', nx.point.row + 1, 'col:', nx.point.column + 1, ')', ' => ', nx.direction)

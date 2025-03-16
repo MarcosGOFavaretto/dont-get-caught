834edb4859e415cf4fe09481aa37c3fa4a30ecbb
Lucas Pultz <70097798+pultzlucas@users.noreply.github.com>
2025-03-16 14:53:31 +0000

    refactor: rename map to grid

--- src/classroom.py
+++ src/classroom.py
@@ -1,4 +1,4 @@
-class ClassroomMapPoint:
+class ClassroomGridPoint:
     def __init__(self, row: int, column: int, x: int, y: int, is_student_desk: bool):
         self.column = column
         self.row = row
@@ -7,30 +7,30 @@ class ClassroomMapPoint:
         self.y = y
     
     def __str__(self):
-        return f"ClassroomMapPoint({self.row}, {self.column}, {self.is_student_desk}, {self.x}, {self.y})"
+        return f"ClassroomGridPoint({self.row}, {self.column}, {self.is_student_desk}, {self.x}, {self.y})"
 
 class Classroom:
     def __init__(self, dimension: tuple[int, int], x: int, y: int, desk_rows: int, desk_columns: int):
         self.width, self.height = dimension
         self.desk_rows = desk_rows
         self.desk_columns = desk_columns
-        self.map_rows = self.desk_rows * 2
-        self.map_columns = (self.desk_columns * 2) - 1
-        self.column_width = self.width / self.map_columns
-        self.row_width = self.height / self.map_rows
+        self.grid_rows = self.desk_rows * 2
+        self.grid_columns = (self.desk_columns * 2) - 1
+        self.column_width = self.width / self.grid_columns
+        self.row_width = self.height / self.grid_rows
         self.x = x
         self.y = y
-        self.map_points = self.get_map_points()
+        self.grid_points = self.get_grid_points()
 
-    # Função para criar um map point a partir de uma posição do mapa de sala
+    # Função para criar um grid point a partir de uma posição do grid
     #
-    def map_point(self, row: int, column: int):
+    def create_grid_point(self, row: int, column: int):
         x = self.column_width * column + self.x
         y = self.row_width * row + self.y
         is_student_desk = self.is_student_desk(row, column)
-        return ClassroomMapPoint(row, column, x, y, is_student_desk)
+        return ClassroomGridPoint(row, column, x, y, is_student_desk)
 
-    # Função que verifica se ponto do mapa de sala é carteira de aluno ou não
+    # Função que verifica se ponto do grid é carteira de aluno ou não
     #
     def is_student_desk(self, row: int, column: int) -> bool:
         return column % 2 == 0 and row % 2 != 0
@@ -44,9 +44,9 @@ class Classroom:
     #   . . . . . . . 
     #   @ . @ . @ . @ 
     #
-    def get_map_points(self):
-        movement_points = list[ClassroomMapPoint]()
-        for r in range(self.map_rows):
-            for c in range(self.map_columns):
-                movement_points.append(self.map_point(r, c))
+    def get_grid_points(self):
+        movement_points = list[ClassroomGridPoint]()
+        for r in range(self.grid_rows):
+            for c in range(self.grid_columns):
+                movement_points.append(self.create_grid_point(r, c))
         return movement_points
--- src/teacher.py
+++ src/teacher.py
@@ -1,4 +1,4 @@
-from .classroom import Classroom, ClassroomMapPoint
+from .classroom import Classroom, ClassroomGridPoint
 import random
 from enum import Enum
 
@@ -9,7 +9,7 @@ class MovementDirection(Enum):
     DOWN = 'DOWN'
 
 class TeacherMovement:
-    def __init__(self, point: ClassroomMapPoint, direction: MovementDirection):
+    def __init__(self, point: ClassroomGridPoint, direction: MovementDirection):
         self.point = point
         self.direction = direction
 
@@ -17,27 +17,27 @@ class TeacherMovement:
         return f"TeacherMovement({self.point}, {self.direction})"
 
 class Teacher:
-    def __init__(self, name: str, classroom: Classroom, initial_position: ClassroomMapPoint):
+    def __init__(self, name: str, classroom: Classroom, initial_position: ClassroomGridPoint):
         self.name = name
         self.classroom = classroom
-        self.current_map_point_position: ClassroomMapPoint = initial_position if initial_position is not None \
-            else classroom.map_point(0, 0)
+        self.current_grid_point_position: ClassroomGridPoint = initial_position if initial_position is not None \
+            else classroom.create_grid_point(0, 0)
 
     # Dentre as possibilidades de movimento possíveis, é escolhida uma aleatoriamente.
     #     
     def next_movement(self) -> TeacherMovement:
         movement_possibilities = self.get_movement_possibilities()
         next_movement_point = random.choice(movement_possibilities)
-        movement_direction = self.get_movement_direction(self.current_map_point_position, next_movement_point)
-        self.current_map_point_position = next_movement_point
+        movement_direction = self.get_movement_direction(self.current_grid_point_position, next_movement_point)
+        self.current_grid_point_position = next_movement_point
         return TeacherMovement(
-            point=self.current_map_point_position,
+            point=self.current_grid_point_position,
             direction=movement_direction,
         )
     
     # Retorna a direção do movimento a partir do ponto inicial e ponto final
     #
-    def get_movement_direction(self, start_point: ClassroomMapPoint, finish_point: ClassroomMapPoint) -> MovementDirection:
+    def get_movement_direction(self, start_point: ClassroomGridPoint, finish_point: ClassroomGridPoint) -> MovementDirection:
         if start_point.row < finish_point.row:
             return MovementDirection.DOWN
         elif start_point.row > finish_point.row:
@@ -50,38 +50,38 @@ class Teacher:
     # Função para calcular o próximo movimento possível do professor:
     #   Regras de movimento:
     #   - O professor pode se mover por todo o corredor da sala (mesmo movimento da torre no xadrez);
-    #   - O professor não pode parar em cima de uma cateira;
+    #   - Uma carteira não pode ser um ponto de movimento;
     #   - O professor não pode pular por cima de uma carteira;
     #
-    def get_movement_possibilities(self) -> list[ClassroomMapPoint]:
-        column_map_points = [mp for mp in self.classroom.map_points if mp.column == self.current_map_point_position.column]
-        row_map_points = [mp for mp in self.classroom.map_points if mp.row == self.current_map_point_position.row]
-        possible_movements = list[ClassroomMapPoint]()
+    def get_movement_possibilities(self) -> list[ClassroomGridPoint]:
+        column_grid_points = [mp for mp in self.classroom.create_grid_points if mp.column == self.current_grid_point_position.column]
+        row_grid_points = [mp for mp in self.classroom.create_grid_points if mp.row == self.current_grid_point_position.row]
+        possible_movements = list[ClassroomGridPoint]()
 
         # up
-        for row_index in range(self.current_map_point_position.row, 0, -1):
-            point = column_map_points[row_index - 1]
+        for row_index in range(self.current_grid_point_position.row, 0, -1):
+            point = column_grid_points[row_index - 1]
             if point.is_student_desk:
                 break
             possible_movements.append(point)
 
         # down
-        for row_index in range(self.current_map_point_position.row + 1, self.classroom.map_rows, 1):
-            point = column_map_points[row_index]
+        for row_index in range(self.current_grid_point_position.row + 1, self.classroom.grid_rows, 1):
+            point = column_grid_points[row_index]
             if point.is_student_desk:
                 break
             possible_movements.append(point)
 
         # left
-        for column_index in range(self.current_map_point_position.column, 0, -1):
-            point = row_map_points[column_index - 1]
+        for column_index in range(self.current_grid_point_position.column, 0, -1):
+            point = row_grid_points[column_index - 1]
             if point.is_student_desk:
                 break
             possible_movements.append(point)
 
         # right
-        for column_index in range(self.current_map_point_position.column + 1, self.classroom.map_columns, 1):
-            point = row_map_points[column_index]
+        for column_index in range(self.current_grid_point_position.column + 1, self.classroom.grid_columns, 1):
+            point = row_grid_points[column_index]
             if point.is_student_desk:
                 break
             possible_movements.append(point)
